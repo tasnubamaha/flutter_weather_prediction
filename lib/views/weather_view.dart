d1e124efcc0b630b427ceb6c23c6e219b797a2d9
tasnubamaha <tasnubamaha267@gmail.com>
2024-09-19 12:44:49 +0600

    do

--- lib/views/weather_view.dart
+++ lib/views/weather_view.dart
@@ -32,28 +32,95 @@ class MainContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const SizedBox(
-          height: 20,
-        ),
-        Lottie.asset(getWeatherAnimation(weatherModel?.weatherCondition)),
-        Text(
-          weatherModel?.weatherCondition ?? "Loading weather condition ...",
-          style: const TextStyle(fontWeight: FontWeight.w100),
-        ),
-        const Spacer(),
-        Text(
-          "${weatherModel?.temperature.round() ?? "Loading Temparature ..."}°C",
-          style: const TextStyle(fontSize: 80, fontWeight: FontWeight.w700),
-        ),
-        Text(
-          weatherModel?.cityName ?? "Loading city ...",
-          style: const TextStyle(fontSize: 40, fontWeight: FontWeight.w100),
-        ),
-        const Spacer(flex: 8),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TodayWeatherContent(weatherModel: weatherModel),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: 5,
+            padding: const EdgeInsets.all(8),
+            itemBuilder: (context, index) => Container(
+              color: const Color(0xFFE8EEF5),
+              height: 80,
+              margin: const EdgeInsets.all(2),
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 80,
+                    height: 80,
+                    child: Lottie.asset(
+                        getWeatherAnimation(weatherModel?.weatherCondition)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          weatherModel!.temperature.round().toString(),
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Text(weatherModel!.cityName),
+                        Text(
+                          weatherModel!.weatherCondition,
+                          style: const TextStyle(
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TodayWeatherContent extends StatelessWidget {
+  const TodayWeatherContent({
+    super.key,
+    required this.weatherModel,
+  });
+
+  final WeatherModel? weatherModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          Lottie.asset(getWeatherAnimation(weatherModel?.weatherCondition)),
+          Text(
+            weatherModel?.weatherCondition ?? "Loading weather condition ...",
+            style: const TextStyle(fontWeight: FontWeight.w100),
+          ),
+          const Spacer(),
+          Text(
+            "${weatherModel?.temperature.round() ?? "Loading Temparature ..."}°C",
+            style: const TextStyle(fontSize: 80, fontWeight: FontWeight.w700),
+          ),
+          Text(
+            weatherModel?.cityName ?? "Loading city ...",
+            style: const TextStyle(fontSize: 40, fontWeight: FontWeight.w100),
+          ),
+          const Spacer(flex: 8),
+        ],
+      ),
     );
   }
 }
